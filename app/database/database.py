55ccb72e8ebb42bd66885795556d89bd60f8f45a
lflynn21 <luketflynn@yahoo.com>
2023-05-02 17:05:30 -0400

    started setting up mongo connection

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,7 @@
 import os
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-# MONGO_URL = os.getenv('MONGO_URL')
+MONGO_URL = os.getenv('MONGO_URL')
 
-# client = AsyncIOMotorClient(MONGO_URL)
+client = MongoClient(MONGO_URL, server_api=ServerApi('1'))
