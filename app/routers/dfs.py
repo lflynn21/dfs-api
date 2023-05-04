c4e113a1d2d7de3e97ff12edd51d2df627ffd5a2
lflynn21 <luketflynn@yahoo.com>
2023-05-04 00:55:35 -0400

    made platform call async

--- app/routers/dfs.py
+++ app/routers/dfs.py
@@ -8,6 +8,6 @@ def list_dfs_platforms():
     return {'dfs_platforms': ['prizepicks']}
 
 @router.get('/{platform}')
-def get_platform_props(platform):
+async def get_platform_props(platform):
     return get_active_props(platform)
 
