9af30c54b73801bb51ce93c70322c2d2aaef7132
lflynn21 <luketflynn@yahoo.com>
2023-05-04 01:03:58 -0400

    added return value validation

--- app/routers/dfs.py
+++ app/routers/dfs.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.database.database import *
+from app.models.models import *
 
 router = APIRouter()
 
@@ -8,6 +9,6 @@ def list_dfs_platforms():
     return {'dfs_platforms': ['prizepicks']}
 
 @router.get('/{platform}')
-async def get_active_platform_props(platform, league=None):
+async def get_active_platform_props(platform, league=None) -> list[Player]:
     return get_active_props(platform, league)
 
