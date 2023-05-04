d6c44e59d84d92f4e0623c106e8250956451ddf0
lflynn21 <luketflynn@yahoo.com>
2023-05-04 19:33:02 -0400

    removed routing for unnecessary calls

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from .routers import sports, books, dfs
 
 app = FastAPI()
 
-app.include_router(sports.router, prefix="/sports")
-app.include_router(books.router, prefix="/books")
+#app.include_router(sports.router, prefix="/sports")
+#app.include_router(books.router, prefix="/books")
 app.include_router(dfs.router, prefix="/dfs")
 
 @app.get("/", tags=['root'])
