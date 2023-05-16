732533603a534169ba0b7aa9230d3ecaf97480db
lflynn21 <luketflynn@yahoo.com>
2023-05-15 23:31:00 -0400

    updated cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import sports, books, dfs
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 #app.include_router(sports.router, prefix="/sports")
 #app.include_router(books.router, prefix="/books")
